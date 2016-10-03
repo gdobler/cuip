b1b23e88d00a8296ed6d5a0867a01495ba29d0a6
mohitsharma44 <mohitsharma44@gmail.com>
2016-10-03 18:02:06 -0400

    get output as a sql.dataframe and save it as a csv file

--- cuip/hadoop/mean.py
+++ cuip/hadoop/mean.py
@@ -1,15 +1,18 @@
 __author__ = "Mohit Sharma"
 
 from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 import numpy as np
 
 APP_NAME = "Image_Mean"
 
-def mean(rdd, stacked, n, nrows, ncols, ndims):
+def mean(sc, rdd, stacked, n, nrows, ncols, ndims):
     """
     Obtain mean of the image
     Parameters
     ----------
+    sc: `SparkContext`
     rdd: `pyspark.rdd` 
         dataset containing binary raw file
         example: rdd = sc.binaryFiles('/path/to/raw/file')
@@ -34,7 +37,7 @@ def mean(rdd, stacked, n, nrows, ncols, ndims):
         mean1 = np.mean(x[:,:,0])
         mean2 = np.mean(x[:,:,1])
         mean3 = np.mean(x[:,:,2])
-        return (mean1,mean2,mean3)
+        return ('ch0',mean1), ('ch1', mean2), ('ch3', mean3)
 
     # For n stacked images, extract individual image and tag them
     for i in range(n):
@@ -46,7 +49,7 @@ def mean(rdd, stacked, n, nrows, ncols, ndims):
     # Distribute the data to all the nodes
     rdd = sc.parallelize(data)
     # Map mean function on every image
-    return rdd.mapValues(_mean).collect()
+    return rdd.mapValues(_mean)
     
 
 if __name__ == "__main__":
@@ -55,6 +58,7 @@ if __name__ == "__main__":
     conf = conf.set("spark.executor.memory", "2g")
     conf = conf.set("spark.executor.cores", "10")
     sc = SparkContext(conf=conf)
+    sqlcontext = SQLContext(sc)
 
     ## Logger
     logger = sc._jvm.org.apache.log4j
@@ -62,10 +66,24 @@ if __name__ == "__main__":
     logger.LogManager.getLogger("akka").setLevel( logger.Level.OFF )
     logger = logger.LogManager.getLogger(__name__)
     logger.info("Obtain Mean intensity of the pixel in every channel")
-
-    # Obtain Mean
-    fname = '/user/mohitsharma44/uo_images/combined.raw'
+    
+    # Obtain Mean        
+    fname = '/user/mohitsharma44/uo_images/*.raw'
     imrdd = sc.binaryFiles(fname)
-    means = mean(rdd=imrdd, stacked=True, n=4, nrows=2160, ncols=4096, ndims=3)
-    logger.info("Means: "+ str(means))
+    means = mean(sc, rdd=imrdd, stacked=True, n=4, nrows=2160, ncols=4096, ndims=3)
 
+    means_converted = means.map(lambda x: (x[0], 
+                                           float(x[1][0][1]), 
+                                           float(x[1][1][1]), 
+                                           float(x[1][2][1])
+                                           ))
+    schema = StructType([StructField("IMG", StringType(), True), 
+                         StructField("CH0", DoubleType(), True), 
+                         StructField("CH1", DoubleType(), True), 
+                         StructField("CH2", DoubleType(), True)
+                         ])
+    df = sqlcontext.createDataFrame(means_converted, schema)
+    # Write it to a file
+    # df.write.csv('mycsv.csv')
+    logger.info("Files from: " +str(fname))
+    logger.info("Means: "+str(df.show()))
