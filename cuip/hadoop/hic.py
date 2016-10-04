677b05116fe29708d885e778f9a002a5aedf655a
mohitsharma44 <mohitsharma44@gmail.com>
2016-10-04 14:43:15 -0400

    removed commented old code

--- cuip/hadoop/hic.py
+++ cuip/hadoop/hic.py
@@ -121,53 +121,6 @@ class HadoopImageCluster(object):
             df = self.sqlcontext.createDataFrame(mean_formatted, schema)
             return df
             
-"""
-def mean(sc, rdd, stacked, n, nrows, ncols, ndims):
-    
-    Obtain mean of the image
-    Parameters
-    ----------
-    sc: `SparkContext`
-    rdd: `pyspark.rdd` 
-        dataset containing binary raw file
-        example: rdd = sc.binaryFiles('/path/to/raw/file')
-    stacked: bool
-        If the rdd contains stacked images (to improve namenode efficiency)
-    n: int
-        if stacked == `True`, n is the number of stacked images
-    nrows: int
-        number of rows per image (per stacked image)
-    ncols: int
-        number of columns per image (per stacked image)
-    ndims: int
-        number of dimensions per image (eg. 3 for RGB image)
-    
-    # Collect the data to create a numpy array.
-    # -- Research a better way -- #
-    img_rdd = rdd.collect()
-    img_series = np.asarray(bytearray(img_rdd[0][1]), dtype=np.uint8)
-    data = []
-
-    def _mean(x): 
-        mean1 = np.mean(x[:,:,0])
-        mean2 = np.mean(x[:,:,1])
-        mean3 = np.mean(x[:,:,2])
-        return ('ch0',mean1), ('ch1', mean2), ('ch3', mean3)
-
-    # For n stacked images, extract individual image and tag them
-    for i in range(n):
-        data.append(
-            ('img'+str(i), 
-             img_series[i*nrows*ncols*ndims : (i+1)*nrows*ncols*ndims].reshape(nrows, ncols, ndims)
-             ))
-    
-    # Distribute the data to all the nodes
-    rdd = sc.parallelize(data)
-    # Map mean function on every image
-    return rdd.mapValues(_mean)
-   
-""" 
-
 if __name__ == "__main__":
     # Obtain Mean        
     f_path = '/user/mohitsharma44/uo_images'
