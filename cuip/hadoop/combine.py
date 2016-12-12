d4db98fa888df6e8f17797cfbe9b0eba414a9def
mohitsharma44 <mohitsharma44@gmail.com>
2016-12-12 18:15:25 -0500

    aesthetic update

--- cuip/hadoop/combine.py
+++ cuip/hadoop/combine.py
@@ -1,123 +1,54 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-__author__ = "Mohit Sharma"
-
 import os
 import multiprocessing
-import psycopg2
 import numpy as np
-from datetime import datetime, timedelta
-from itertools import islice
-from scipy.ndimage import imread 
+from datetime import datetime
+from scipy.ndimage import imread
 from cuip.cuip.utils import cuiplogger
 from cuip.cuip.utils.misc import get_files, _get_files
 
-# initialize logger
-logger = cuiplogger.cuipLogger(loggername="BATCH", tofile=False)
+logger = cuiplogger.cuipLogger(loggername="COMBINE", tofile=False)
 
-
-class Worker(multiprocessing.Process):
+# -- define the function for stacking images
+def merge_subset(conn, sublist, dpath, binfac, nimg_per_file, nrow=2160, 
+                 ncol=4096, nwav=3, verbose=False):
     """
-    Takes the next task from task pool/queue and executes it.
+    Take a list of lists, merge each sublist into a stacked image, and 
+    write to disk.
     """
+    dr      = nrow//binfac
+    dc      = ncol//binfac
+    img_out = np.zeros([nimg_per_file*dr, dc, nwav], dtype=np.uint8)
 
-    def __init__(self, task_queue, result_queue):
-        multiprocessing.Process.__init__(self)
-        self.task_queue   = task_queue
-        self.result_queue = result_queue
-
-    def run(self):
-        proc_name = self.name
-        while True:
-            next_task = self.task_queue.get()
-            if next_task is None:
-                # Poison pill means shutdown
-                logger.info('%s: Exiting' % proc_name)
-                self.task_queue.task_done()
-                break
-            answer = next_task()
-            self.task_queue.task_done()
-            self.result_queue.put(answer)
-        return
-
-
-class StackImages(object):
-    """
-    Context around a function that stacks images.
-    """
-
-    def __init__(self, img_arr=None, filelist=None, fac=1, nrow=None, 
-                 ncol=None, nwav=None, outpath=None):
-        self.img_arr  = img_arr
-        self.filelist = filelist
-        self.fac      = fac
-        self.nrow     = nrow
-        self.ncol     = ncol
-        self.nwav     = nwav
-        self.outpath  = outpath
-
-    def __call__(self):
-        self.combine_images(self.img_arr, self.filelist, self.fac, self.nrow, 
-                            self.ncol, self.nwav, self.outpath)
-
-    def combine_images(self, img_arr=None, filelist=None, fac=1, nrow=None, 
-                       ncol=None, nwav=None, outpath=None):
-        """
-        Convert raw/ png images to raw files
-        Parameters
-        ----------
-        img_arr: np.array
-            numpy array of the size of the image
-            example:
-        img_arr = np.zeros([nrow*n, ncol, nwav], np.uint8)
-            where `n` is the total number of images to be combined
-            by stacking vertically
-        filelist: list
-            list containing files to be stacked together
-        fac: int
-            binning factor. default is 1
-        nrow: int
-            number of rows in a single image
-        ncol: int
-            number of cols in a single image
-        nwav: int
-            number of dimensions (RGB = 3)
-        outpath: str
-            path where the file should be written to
-        """
-
-        for ind, tfile in enumerate(filelist):
-            lo  = (nrow/fac)*ind
-            hi  = (nrow/fac)*(ind+1)
+    for tflist in sublist:
+        for ii,tfile in enumerate(tflist):
             ext = tfile[-3:].lower()
-
             if ext == 'png':
-                img_arr[lo:hi] = imread(tfile, mode='RGB')[::fac, ::fac]
+                img_out[dr*ii:dr*(ii+1)] = imread(tfile, mode="RGB") \
+                    [::binfac, ::binfac]
             elif ext == 'raw':
-                img_arr[lo:hi] = np.fromfile(tfile, dtype=np.uint8) \
-                    .reshape(nrow, ncol, nwav)[::fac, ::fac]
+                img_out[dr*ii:dr*(ii+1)] = np.fromfile(tfile, dtype=np.uint8) \
+                    .reshape(nrow, ncol, nwav)[::binfac, ::binfac]
             else:
-                logger.error("File Format not supported "+str(tfile))
+                logger.error("File format not supported "+str(tfile))
 
-        # rename the extension of the file to .raw
-        newfname = str(os.path.getmtime(tfile))+".raw"
+        newfname = os.path.join(dpath, os.path.basename(tflist[0]))[:-3]+"raw"
 
-        # write to file
-        logger.info("Writing: "+str(newfname))
-        img_arr.tofile(os.path.join(outpath, newfname))
+        logger.info("Writing: "+newfname)
+        img_out.tofile(newfname)
+        img_out[:] = 0
 
+    return
 
 if __name__ == "__main__":
-    # setting up threadsafe tasks and results queue
-    tasks   = multiprocessing.JoinableQueue()
-    results = multiprocessing.Queue()
 
-    # set input and output paths
+    # -- get the file list
     inpath  = os.getenv("CUIP_2013")
     outpath = "output/combined_images"
     dbname  = os.getenv("CUIP_DBNAME")
-
+        
     # set start and end times
     st_date = "2013.11.17"
     st_time = "17.00.00"
@@ -126,54 +57,57 @@ if __name__ == "__main__":
 
     st = datetime(*[int(i) for i in st_date.split(".") + st_time.split(".")])
     en = datetime(*[int(i) for i in en_date.split(".") + en_time.split(".")])
+
+    # -- get all the files between st and en
+    if dbname:
+        logger.info("Fetching file locations from database")
+        file_list = get_files(dbname, st, en)
+    else:
+        logger.warning("Database not found. Process continue by scanning filesystem")
+        logger.warning("This might take longer")
+        # get files by scanning the file system 
+        file_gen_list = _get_files(inpath, st, en)
+        for all_files in file_gen_list:
+            file_list.append(all_files)
+
+    nin = len(file_list)
+
+    # -- set the binning and determine the number of output files
+    binfac        = 2
+    nimg_per_file = 4 * binfac * binfac
+    nout          = nin // nimg_per_file + 1*((nin % nimg_per_file) > 0)
+
+    # -- partition the file list into output files and processors
+    flist_out = [file_list[i*nimg_per_file:(i+1)*nimg_per_file] for i in range(nout)]
+
+    # -- set the number of processors
+    nproc = 3
+    logger.info("Creating %s worker processes"%(nproc))
+
+    # -- set the number of files per processor
+    if nproc == 1:
+        nout_per_proc = nout 
+    elif nout % nproc == 0:
+        nout_per_proc = nout//nproc
+    else:
+        nout_per_proc = nout//nproc + 1
+
+    # -- initialize workers and execute
+    parents, childs, ps = [], [], []
     
-    # Files to be comined together
-    combine = 64
-    fac     = 4
+    for ip in range(nproc):
+        ptemp, ctemp = multiprocessing.Pipe()
+        parents.append(ptemp)
+        childs.append(ctemp)
+        
+        lo = ip * nout_per_proc
+        hi = (ip+1) * nout_per_proc
+        ps.append(multiprocessing.Process(target=merge_subset, 
+                                          args=(childs[ip], flist_out[lo:hi], 
+                                                outpath, binfac, nimg_per_file), 
+                                          kwargs={"verbose":True}))
+        
+        ps[ip].start()
 
-    # File shapes
-    nrow = 2160
-    ncol = 4096
-    nwav = 3
-
-    # Empty Array
-    comb_imgs = np.zeros([nrow*combine/fac, ncol/fac, nwav], np.uint8)
-    
-    # Start workers
-    num_workers = 20
-    logger.info('Creating %d Workers' % num_workers)
-    workers = [Worker(tasks, results) for i in range(num_workers)]
-
-    for worker in workers:
-        worker.start()
-
-    def poisonChildren():
-        # Add a poison pill for each worker
-        for i in range(num_workers):
-            tasks.put(None)
-            
-    # put jobs (tasks) in queue
-    try:
-        file_list = []
-        # first try to get files from database
-        if dbname:
-            logger.info("Fetching file locations from database")
-            file_list = get_files(dbname, st, en)
-        else:
-            logger.warning("Database not found. Process continue by scanning filesystem")
-            logger.warning("This might take longer")
-            # get files by scanning the file system
-            file_gen_list = _get_files(inpath, st, en)
-            for all_files in file_gen_list:
-                file_list.append(all_files)
-        # group of 'combine' files
-        files = zip(*(iter(file_list),) * combine)
-        for n_files in files:
-            tasks.put(StackImages(comb_imgs, n_files, 
-                                  fac, nrow, ncol, nwav,
-                                  outpath))
-        logger.info("All Tasks Added")
-    finally:
-        poisonChildren()
-        # Wait for all of the tasks to finish
-        tasks.join()
+    # -- Join all processes
+    dum = [ps[ip].join() for ip in range(nproc)]
