ea693dfa5bb0ba2b18ba08c8714965bf8a52445a
mohitsharma44 <mohitsharma44@gmail.com>
2017-01-09 12:12:49 -0500

    using compund keys for uo_files, indexing by timestamp and class for updating table

--- cuip/database/add_files.py
+++ cuip/database/add_files.py
@@ -1,6 +1,6 @@
 import os
 from datetime import datetime
-from sqlalchemy import exc
+from sqlalchemy import exc, update
 from cuip.cuip.utils import cuiplogger
 from cuip.cuip.database.db_tables import ToFilesDB
 
@@ -23,8 +23,7 @@ class AddTask(object):
                                 fpath=os.path.dirname(f),
                                 fsize=int(os.path.getsize(f)/1024**2.),
                                 mean=0, std=0, bright_pix=0,
-                                timestamp=datetime.fromtimestamp(
-                        os.path.getmtime(f)).replace( microsecond=0),
+                                timestamp=datetime.fromtimestamp(os.path.getmtime(f)),
                                 cloud='',
                                 visibility=-1,
                                 roffset=0, coffset=0, angle=0,
@@ -34,3 +33,28 @@ class AddTask(object):
             except exc.IntegrityError:
                 logger.warning("Duplicate Entry found for "+str(os.path.basename(f)))
                 session.rollback()
+
+class UpdateTask(object):
+    
+    def __init__(self, flist, values_to_update):
+        """
+        Parameters
+        ----------
+        flist: list
+            list of filenames
+        values_to_update: dict
+            key value pair of column/ parameter to update in database table
+        """
+        self.flist = flist
+        self.values_to_update = values_to_update
+
+    def __call__(self, session=None, *args, **kwargs):
+        """
+        update database
+        """
+        try:
+            update(ToFilesDB).where(ToFilesDB.fname.in_(map(lambda x: x.split('/')[-1], self.flist))).values(self.values_to_update)
+            session.commit()
+        except exc.IntegrityError:
+            logger.warning("Duplicate Entry found for "+str(os.path.basename(f)))
+            session.rollback()
