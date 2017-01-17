8b9e529a4e3c9cd69c1b7471d514836a4f2727c1
Federica Bianco <fbianco@compute.cusp.nyu.edu>
2017-01-17 16:20:54 -0500

    adding parallelized version im working on

--- cuip/plumes/sub_foregroundFed.py
+++ cuip/plumes/sub_foregroundFed.py
@@ -11,9 +11,9 @@ from joblib import Parallel, delayed
 from joblib.pool import has_shareable_memory
 #from plm_images import *
 
-MAXPROCESSES = 5
+MAXPROCESSES = 5e6
 OUTPUTDIR = "./outputs/"
-LIM = 15
+LIM = -1 #25
 
 try:
     DST_WRITE = os.environ['DST_WRITE']
@@ -43,62 +43,78 @@ class RawImages:
                     self.imsize['nbands']).astype(float)
         return rgb
 
-def subforg(frames):
+def subforg(difs, frames, i, j):
 #image loop
     #lf must be odd
-    #frames = range(indx - boxsize, ii) + range(indx + 1, ii + boxsize + 1)
+
+    fulllen = np.prod(frames[0].shape)
+    frames = frames[i-j:i+j+1]
     lf = int(len(frames) / 2)
-    print('working on {0}'.format(ii), frames)
+    #print('working on {0}'.format(i))
     dif = (- 1.0 * np.concatenate([frames[:lf], 
                                    frames[(lf+1):]]) + 1.0 * frames[lf])
-    #for jj, fr in enumerate(frames):
-    #    dif[jj] = 1.0 * raw.imgs[ii] - 1.0 * raw.imgs[fr]
-    diffs = abs(dif).min(0)
-    return diffs
+    ##for jj, fr in enumerate(frames):
+    ##    dif[jj] = 1.0 * raw.imgs[ii] - 1.0 * raw.imgs[fr]
+    
+    tmp = abs(dif).min(0).flatten()
+    #difs[i*fulllen: i*fulllen+len(tmp)] = tmp
+    difs.tofile(OUTPUTDIR + "/" + fname + '_%04d.npy'%i)
 
-# -- get the file list
-#fl = pkl.load(open(os.path.join(os.environ['DST_WRITE'],'filelist.pkl'),'rb'))
-fl = (open(os.path.join(DST_WRITE, 'filelist.txt'), 'rb')).readlines()
+if __name__=='__main__':
 
-# -- get the raw images
-raw = RawImages(fl=fl, lim=LIM)
-# set the image numbers
-lim = LIM-5 if LIM > 0 else len(fl)-5
+    # -- get the file list
+    #fl = pkl.load(open(os.path.join(os.environ['DST_WRITE'],'filelist.pkl'),'rb'))
+    fl = (open(os.path.join(DST_WRITE, 'filelist.txt'), 'rb')).readlines()
 
-# -- initialize the the difference image list
-difs = np.zeros([lim] + list(raw.imgs[0].shape))
-dif  = np.zeros([10] + list(raw.imgs[0].shape))
+    # -- get the raw images
+    raw = RawImages(fl=fl, lim=LIM)
+    # set the image numbers
+    lim = LIM-5 if LIM > 0 else len(fl)-5
 
+    # -- initialize the the difference image list
+    difs = np.zeros([lim] + list(raw.imgs[0].shape))
+    difs = mpc.Array('f', difs.flatten())
 
-# -- loop through the images
-nps = min(mpc.cpu_count() - 1 or 1, MAXPROCESSES)
-#pool = mpc.Pool(processes=nps)
-print([raw.imgs[i-5:i+6].flatten().shape for i in range(5, lim)])
+    # -- loop through the images
+    nps = min(mpc.cpu_count() - 1 or 1, MAXPROCESSES)
+
+    #pool = mpc.Pool(processes=nps)
+#print([raw.imgs[i-5:i+6].flatten().shape for i in range(5, lim)])
 #sys.exit()
 #tmp = pool.map(subforg, itertools.izip([raw.imgs[i-5:i+6] for i in range(5, lim)], 
 #              itertools.repeat(5)))
-#pool.close()
-#pool.join()
+#pool.close
+    print("before ",difs[6])
+    processes = [mpc.Process(target=subforg, args=(difs, raw.imgs, i, 5)) for i in range(5, lim)]
 
-Parallel(n_jobs=nps)(
-        delayed(sum(raw.imgs[i-5:i+6]) for i in range(5, lim)))
-tmp = np.zeros(100)
-difs = tmp 
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
+
+    
+    #difs = np.array(difs[:]).reshape([lim] + list(raw.imgs[0].shape))
+    print("after", difs[6])
+
+#Parallel(n_jobs=nps)(
+#        delayed(sum(raw.imgs[i-5:i+6]) for i in range(5, lim)))
+#tmp = np.zeros(100)
+#difs = tmp
 
 #calc, itertools.izip(range(NM0, nm), itertools.repeat(second_args)))  # for i in range(nm): result[i] = f(i, second_args)
 
 #for ii in range(5, lim):
 #    difs[ii] = subforg(ii, raw, difs[ii])
-#difs = difs[5:]
-
+    difs = difs[5:]
+'''
 # -- save figures to file
 im = pl.imshow(difs[0][:,:,0],clim=[0,5])
 for ii in range(len(difs)):
-    im.set_data(difs[ii][:,:,0])
-    im.set_clim([0,5])
-    pl.draw()
-    pl.show()
-    pl.savefig(OUTPUTDIR + '/difs_'+str(ii).zfill(3)+'.png',clobber=True)
+im.set_data(difs[ii][:,:,0])
+im.set_clim([0,5])
+pl.draw()
+pl.show()
+pl.savefig(OUTPUTDIR + '/difs_'+str(ii).zfill(3)+'.png',clobber=True)
 
 
 # -- save panel figures to file
@@ -112,14 +128,14 @@ pl.axis('off')
 pl.subplots_adjust(0.05,0.05,0.95,0.95,0.05,0.05)
 #for ii,jj in enumerate(range(28,42)):
 for ii,jj in enumerate(range(5, lim)):
-    imt.set_data(raw.imgs[jj])
-    pl.draw()
-    imb.set_data(difs[ii][:,:,0])
+imt.set_data(raw.imgs[jj])
+pl.draw()
+imb.set_data(difs[ii][:,:,0])
 #    imb.set_data(difs[jj-5][:,:,0])
-    pl.clim([0,5])
-    pl.draw()
-    pl.savefig(OUTPUTDIR + '/difs_imgs_' + str(ii).zfill(3) + 
-               '.png',clobber=True)
+pl.clim([0,5])
+pl.draw()
+pl.savefig(OUTPUTDIR + '/difs_imgs_' + str(ii).zfill(3) + 
+'.png',clobber=True)
 
-
+'''
 
