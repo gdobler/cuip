9f16c58db57fa6a9bc71895421340f22c7f75aa4
Greg Dobler <greg.dobler@gmail.com>
2017-02-10 13:27:52 -0500

    skeleton for the LightCurves class

---
+++ cuip/lightcurves/lightcurves.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class LightCurves(object):
+    """
+    Lightcurves from a night of observations by the CUSP Urban Observatory.
+    """
+
+    def __init__(self, st, en):
+        """
+        Return an object containing light curves from the start to end time.
+        """
+
+        return
+
+    def get_file_list(self):
+        """
+        Get the full file list for all files between the start and end time.
+        """
+
+        print("'get_file_list' METHOD NOT IMPLEMENTED YET!!!")
+
+        return
+
+    def gen_light_curves(self):
+        """
+        Generate light curves.
+        """
+
+        print("'gen_light_curves' METHOD NOT IMPLEMENTED YET!!!")
+
+        # -- get the full file 
+
+        return
+
+    @staticmethod
+    def get_registration(iden):
+        """
+        Check if this image is registered and return the registration
+        parameters if so.
+        """
+
+        print("'check_registration' STATIC METHOD NOT IMPLEMENTED YET!!!")
+
+        return
