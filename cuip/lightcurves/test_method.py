cd88cdd73981786eb5c3abc5cc0d7f0a53471dbd
Greg Dobler <greg.dobler@gmail.com>
2017-02-20 21:16:13 -0500

    updated

--- cuip/lightcurves/test_method.py
+++ cuip/lightcurves/test_method.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import time
 import numpy as np
 from scipy.ndimage.measurements import label, mean
 from cuip.cuip.registration.register import get_reference_image
@@ -12,8 +13,9 @@ ncol = 4096
 buff = 20
 
 # -- get source mask
+labs = os.path.join(os.getenv("CUIP_SUPPLEMENTARY"), "window_labels.out")
 srcs = np.zeros((nrow, ncol), dtype=bool)
-srcs[buff:-buff, buff:-buff] = np.fromfile("window_labels.out", int) \
+srcs[buff:-buff, buff:-buff] = np.fromfile(labs, int) \
     .reshape(nrow - 2 * buff, ncol - 2* buff) \
     .astype(bool)
 
@@ -24,5 +26,7 @@ labs = label(srcs)
 ref = get_reference_image()
 
 # -- get means
+t0 = time.time()
 vals = np.array([mean(ref[..., i], labs[0], range(1, labs[1]+1)) for i in 
                  [0, 1, 2]]).T
+print("Brightness extraction time {0}".format(time.time() - t0))
