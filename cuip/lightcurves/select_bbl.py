15f055206f0426d104ecae68c97303c2bf990dae
Greg Dobler <greg.dobler@gmail.com>
2017-02-28 23:22:22 -0500

    a mini-gui for selecting a BBL with a click

---
+++ cuip/lightcurves/select_bbl.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from cuip.cuip.registration.register import get_reference_image
+
+def select_bbl():
+    """
+    Return BBL at selected position.
+    """
+
+    # -- get reference image
+    ref = get_reference_image()
+
+    # -- get the BBL map
+    buff  = 20
+    bname = os.path.join("..", "data", "12_3_14_bblgrid_clean.npy")
+    bmap  = np.zeros((ref.shape[0], ref.shape[1]))
+    bmap[buff:-buff, buff:-buff] = np.load(bname)
+
+    # -- initialize the plot
+    xs = 8.0
+    ys = xs * float(ref.shape[0]) / float(ref.shape[1])
+    fig, ax = plt.subplots(figsize=(xs, ys))
+    fig.subplots_adjust(0, 0, 1, 1)
+    ax.axis("off")
+    im = ax.imshow(ref)
+    plt.show()
+
+    # -- select
+    cind, rind = [int(round(i)) for i in ginput(1, -1)[0]]
+
+    return int(bmap[rind, cind])
