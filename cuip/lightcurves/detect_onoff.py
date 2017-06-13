42917fde534ee63b572169d4e58d74823d133d15
Greg Dobler <greg.dobler@gmail.com>
2017-06-12 22:57:57 -0400

    cross check for statistical significance

--- cuip/lightcurves/detect_onoff.py
+++ cuip/lightcurves/detect_onoff.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter as gf
 from scipy.ndimage import correlate1d
-execfile("split_days.py")
+from split_days import *
 
 # -- utilities
 file_index   = 0
@@ -13,9 +13,10 @@ width        = 30
 delta        = 2
 sig_clip_amp = 2.0
 sig_peaks    = 10.0
+sig_xcheck   = 2.0
 
 # -- read in lightcurves and convert to grayscale
-print("reading lightcurves for nights index {0}".format(file_index))
+print("reading lightcurves for nights index {0}...".format(file_index))
 lcs = np.load(os.path.join("output", "light_curves_{0:04}.npy" \
                                .format(file_index))).mean(-1)
 
@@ -29,14 +30,17 @@ msk_sm = gf(msk.astype(float), (width, 0))
 lcs_sm = gf(lcs * msk, (width, 0)) / (msk_sm + (msk_sm == 0))
 
 # -- compute the gaussian difference (using a masked array now)
+print("computing gaussian differences...")
 lcs_gd = np.ma.zeros(lcs_sm.shape, dtype=lcs_sm.dtype)
 lcs_gd[delta // 2: -delta // 2] = lcs_sm[delta:] - lcs_sm[:-delta]
 
 # -- set the gaussian difference mask
+print("resetting the mask...")
 lcs_gd.mask = np.zeros_like(msk)
 lcs_gd.mask[delta // 2: -delta // 2] = ~(msk[delta:] * msk[:-delta])
 
 # -- get the indices of the date separators and create the individual dates
+print("splitting dates...")
 dind_lo = list(split_days(file_index))
 dind_hi = dind_lo[1:] + [lcs_gd.shape[0]]
 nights  = [lcs_gd[i:j] for i, j in zip(dind_lo, dind_hi)]
@@ -46,7 +50,7 @@ nnights = len(nights)
 avgs = []
 sigs = []
 for ii in range(nnights):
-    print("sigma clipping night {0} of {1}".format(ii + 1, nnights))
+    print("sigma clipping night {0} of {1}...".format(ii + 1, nnights))
     tmsk = nights[ii].mask.copy()
     for _ in range(10):
         avg             = nights[ii].mean(0)
@@ -74,19 +78,26 @@ for ii in range(nnights):
                           ~nights[ii].mask[1:-1]
 
 # -- cross check left/right means for robustness to noise
+print("setting up filters...")
 mean_diff  = ((np.arange(2 * width) >= width) * 2 - 1) / float(width)
 mean_left  = 1.0 * (np.arange(2 * width) < width) / float(width)
 mean_right = 1.0 * (np.arange(2 * width) >= width) / float(width)
-lcs_md     = correlate1d(lcs, mean_diff, axis=0)
+print("calculating mean difference across transition...")
+lcs_md     = np.abs(correlate1d(lcs, mean_diff, axis=0))
+print("calculating max standard deviation across transition...")
 lcs_sq     = lcs**2
 lcs_std    = np.sqrt(np.maximum(correlate1d(lcs_sq, mean_left, axis=0) - 
                                 correlate1d(lcs, mean_left, axis=0)**2, 
                                 correlate1d(lcs_sq, mean_right, axis=0) - 
                                 correlate1d(lcs, mean_right, axis=0)**2))
 
-
-bar = lcs_md > sig_xcheck * lcs_std
-
+# -- identify all potentially robust transitions and prune list
+#    NB: careful about handling partial initial days.
+print("pruning ons and offs for statistical significance...")
+pad       = np.zeros((dind_lo[0], lcs.shape[1]), dtype=bool)
+good_arr  = lcs_md > sig_xcheck * lcs_std
+good_ons  = np.vstack([pad] + tags_on) & good_arr
+good_offs = np.vstack([pad] + tags_off) & good_arr
 
 
 def canny1d(lcs, indices=None, width=30, delta=2, see=False, sig_clip_iter=10, 
