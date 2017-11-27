9f72291996604bd4509cda2a41e19c7039e3b443
Jordan Vani <jmv423@nyu.edu>
2017-11-27 14:53:45 -0500

    Boxplots with residential split and light sources as res or not.

--- cuip/variability/plot.py
+++ cuip/variability/plot.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
+import scipy.ndimage.measurements as ndm
 
 plt.style.use("ggplot")
 
@@ -16,24 +17,23 @@ def plot_night(lc):
     # -- Get data for the given night.
     dimg = lc.lightc
     dimg[dimg == -9999.] = np.nan
-    dimg = (dimg - np.nanmin(dimg, axis=0)) / (np.nanmax(dimg, axis=0) - np.nanmin(dimg, axis=0))
+    dimg = ((dimg - np.nanmin(dimg, axis=0)) /
+            (np.nanmax(dimg, axis=0) - np.nanmin(dimg, axis=0)))
     dimg = dimg.T
-    offs = lc.bigoffs[lc.night]
+    offs = lc.bigoffs.loc[lc.night].sort_values()
 
-    # -- Pull x and y coords (i.e., off and source),
-    vals = zip(map(lambda x: x[1], offs), map(lambda x: x[0], offs))
-    # -- Sort by off timestep.
-    vals = sorted(vals, key=lambda x: x[0])
     # -- Sort dimg by off time.
+    vals = zip(offs.values, offs.index)
     xx, yy = zip(*vals)
     dimg = dimg[[yy]]
+
     # -- Only plot off times > 0.
     valsf = filter(lambda x: x[0] > 0, vals)
     xx, yy = zip(*valsf)
 
     fig, ax = plt.subplots(figsize=(12, 6))
     ax.imshow(dimg, aspect="auto")
-    ax.scatter(xx, np.arange(len(xx)) + (len(vals) - len(valsf)), marker="x", s=4)
+    ax.scatter(xx, np.arange(len(xx)), marker="x", s=4)
     ax.set_ylim(0, dimg.shape[0])
     ax.set_xlim(0, dimg.shape[1])
     ax.set_title("Light Curves for {}".format(lc.night))
@@ -57,7 +57,7 @@ def plot_lightcurve(lc, idx):
     dimg[dimg == -9999.] = np.nan
     offs = lc.off.T
     ons = lc.on.T
-    bigoff_ = lc.bigoffs[lc.night][idx][1]
+    bigoff_ = lc.bigoffs.loc[lc.night].loc[idx]
     bigoff = (bigoff_ if bigoff_ > 0 else None)
 
     fig, ax = plt.subplots(figsize=(9, 3))
@@ -82,7 +82,7 @@ def plot_lightcurve(lc, idx):
     plt.show(block=True)
 
 
-def plot_winter_summer_bigoffs_boxplot(lc, median=True):
+def plot_winter_summer_bigoffs_boxplot(lc, median=True, res=True):
     """Plot boxplots comparing bigoffs for summer and winter observations.
     Args:
         lc (obj) - LightCurve object.
@@ -90,13 +90,24 @@ def plot_winter_summer_bigoffs_boxplot(lc, median=True):
     """
 
     if median:
-        winter = lc.df[lc.df.index.month > 9].median(axis=1).dropna()
-        summer = lc.df[lc.df.index.month < 9].median(axis=1).dropna()
+        winter = lc.bigoffs[lc.bigoffs.index.month > 9]
+        summer = lc.bigoffs[lc.bigoffs.index.month < 9]
         centralm = "Median"
     else:
-        winter = lc.df[lc.df.index.month > 9].mean(axis=1).dropna()
-        summer = lc.df[lc.df.index.month < 9].mean(axis=1).dropna()
+        winter = lc.bigoffs[lc.bigoffs.index.month > 9]
+        summer = lc.bigoffs[lc.bigoffs.index.month < 9]
         centralm = "Mean"
+
+    if res:
+        res_labs = filter(lambda x: lc.coords[x][1] > 1020, lc.coords.keys())
+        winter = winter[res_labs].median(axis=1).dropna()
+        summer = summer[res_labs].median(axis=1).dropna()
+        title = "{} {} Bigoff Timestep for Summer and Winter" \
+            .format(centralm, "Residential")
+    else:
+        winter = winter.median(axis=1).dropna()
+        summer = summer.median(axis=1).dropna()
+        title = "{} Bigoff Timestep for Summer and Winter".format(centralm)
 
     fig, ax = plt.subplots(figsize=(8, 2))
     ax.boxplot([winter.values, summer.values],
@@ -104,7 +115,32 @@ def plot_winter_summer_bigoffs_boxplot(lc, median=True):
 
     ax.set_ylim(-0.1, 0.3)
     ax.set_xlabel("Timesteps")
-    ax.set_title("{} Bigoff Timestep for Summer and Winter".format(centralm))
+    ax.set_title(title)
 
+    plt.tight_layout()
+    plt.show(block=True)
+
+
+def plot_light_sources(lc):
+    """Plot residential/commercial split of appertures and overlay apperture
+    centers.
+    Args:
+        lc (obj) - LightCurve object.
+    """
+
+    xx, yy = zip(*map(lambda x: lc.coords[x], lc.coords.keys()))
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.imshow(lc.labs)
+    ax.imshow(lc.labs[:1020], cmap="terrain")
+    ax.scatter(xx, yy, c="r", s=2, marker="x")
+
+    ax.set_title("Residential/Commercial Split of Appertures (with Centers)")
+    ax.set_ylim(0, lc.labs.shape[0])
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid("off")
+
+    plt.gca().invert_yaxis()
     plt.tight_layout()
     plt.show()
