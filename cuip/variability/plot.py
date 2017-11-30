957ec1c24f335eb00b4087ff43d9baadf6fbf374
Jordan Vani <jmv423@nyu.edu>
2017-11-30 11:36:28 -0500

    Convert bbls map to PLUTO bldgclass and higher level class.

--- cuip/variability/plot.py
+++ cuip/variability/plot.py
@@ -1,13 +1,33 @@
 from __future__ import print_function
 
+import cPickle
+import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import ttest_ind
+import matplotlib.patches as mpatches
 import scipy.ndimage.measurements as ndm
 
 plt.style.use("ggplot")
 
 
-def plot_night(lc):
+def read_img(file_path):
+    """Read .raw or .png image file.
+    Args:
+        file_path (str) - path to raw image.
+    Returns:
+        img (array) - image as np array."""
+
+    if file_path.endswith(".raw"):
+        img = np.fromfile(file_path, dtype=np.uint8) \
+                .reshape(2160, 4096, 3)[...,::-1]
+
+    if file_path.endswith(".png"):
+        img = imread(file_path)
+
+    return img
+
+
+def plot_night_img(lc):
     """Plot image of all lightcurves on the loaded evening. Sort by bigoff time
     and overlay bigoff times.
     Args:
@@ -44,7 +64,7 @@ def plot_night(lc):
     plt.show(block=True)
 
 
-def plot_lightcurve(lc, idx):
+def plot_lightcurve_line(lc, idx):
     """Plot single lightcurve from the loaded evening and overlay with all ons,
     offs, and the bigoff.
     Args:
@@ -121,7 +141,7 @@ def plot_winter_summer_bigoffs_boxplot(lc, median=True, res=True):
     plt.show(block=True)
 
 
-def plot_light_sources(lc):
+def plot_appertures(lc):
     """Plot residential/commercial split of appertures and overlay apperture
     centers.
     Args:
@@ -165,3 +185,170 @@ def plot_imshow_lightc(lc, show=True):
         plt.show(block=True)
     else:
         plt.savefig("./pdf/lightc_{}.png".format(lc.night))
+
+
+def plot_bbls(lc, img_path=os.environ["EXAMPLEIMG"]):
+    """Plot bbls, example img, and sources.
+    Args:
+        lc (obj) - LightCurve object.
+        img_path (str) - path to example image.
+    """
+
+    # -- Get paths.
+    bbl_path = os.path.join(lc.spath, "12_3_14_bblgrid_clean.npy")
+
+    # -- Find drow and dcol for the example image.
+    spath = img_path.split("/")
+    yyyy, mm, dd = int(spath[5]), int(spath[6]), int(spath[7])
+    fnum = lc.meta.loc[pd.datetime(yyyy, mm, dd).date()]["fname"]
+    reg = pd.read_csv(os.path.join(lc.rpath, "register_{}.csv".format(fnum)))
+    img_reg = reg[reg.fname == os.path.basename(img_path)]
+    drow = img_reg.drow.values.round().astype(int)[0]
+    dcol = img_reg.dcol.values.round().astype(int)[0]
+
+    # -- Load bbls, and replace 0s.
+    bbls = np.load(bbl_path)
+    np.place(bbls, bbls == 0,  np.min(bbls[np.nonzero(bbls)]) - 100)
+    # -- Convert coords from dict to x and y lists.
+    xx, yy = zip(*lc.coords.values())
+    # -- Load image without buffer.
+    img = read_img(img_path)[20:-20, 20:-20]
+
+    fig, ax = plt.subplots(figsize=(12, 4))
+    # -- Plot image, rolling by drow and dcol.
+    ax.imshow(np.roll(img, (drow, dcol), (0, 1)))
+    ax.imshow(bbls, alpha=0.3, vmax=1013890001, cmap="flag_r")
+    # -- Scatter light source coordinates taking buffer into consideration.
+    uniq = np.unique(lc.coord_bbls.values())
+    cmap = dict(zip(uniq, range(len(uniq))))
+    colors = [cmap[key] for key in lc.coord_bbls.values()]
+    ax.scatter(np.array(xx) - 20, np.array(yy) - 20, marker="x", c=colors, s=3,
+        cmap="flag_r")
+
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid("off")
+    plt.show()
+
+
+def plot_bldgclass(lc):
+    """Plot PLTUO BldgClass.
+    Args:
+        lc (obj) - LightCurve object.
+    """
+
+    # -- Path to bldgclass.np
+    bldgclass_path = os.path.join(lc.outp, "bldgclass.npy")
+
+    # -- Load bbls, and replace 0s.
+    bbls = np.load(lc.bbl_path)
+    np.place(bbls, bbls == 0,  np.min(bbls[np.nonzero(bbls)]) - 100)
+
+    # -- Map BBL to BldgClass.
+    try:
+        bldgclass = np.load(bldgclass_path)
+    else:
+        print("bldgclass.npy does not exist!")
+        bldgclass = np.array([lc.bbln[bbl] if bbl in lc.bbln.keys() else -1
+            for bbl in bbls.ravel()]).reshape(bbls.shape[0], bbls.shape[1])
+
+    bldgclass = bldgclass.astype(float)
+    bldgclass[bldgclass == -1] = np.nan
+
+    # -- Plot img.
+    fig, ax = plt.subplots(figsize=(16, 8))
+    im = ax.imshow(bldgclass, cmap="tab20b")
+    ax.set_title("Building Class")
+    ax.set_facecolor("w")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid("off")
+
+    # -- Create legend.
+    values = np.unique(bldgclass)
+    values = values[values < 100]
+    colors = [im.cmap(im.norm(value)) for value in values]
+    patches = [mpatches.Patch(color=colors[int(i)],
+        label="{}".format(lc.classn_r[int(i)])) for i in values[1:]]
+    plt.legend(handles=patches, ncol=17, prop={"size": 7}, frameon=False)
+    plt.tight_layout()
+    plt.show()
+
+
+def create_arbclass_dict(lc):
+    """Categorize BldgClass numbers.
+    Args:
+        lc (obj) - LightCurve object.
+    """
+
+    arbclass = {}
+    res = ["B", "C", "D", "N", "R1", "R2", "R3", "R4", "S"]
+    com = ["J", "K", "L", "O", "RA", "RB", "RC", "RI"]
+    mix = ["RM", "RR", "RX"]
+    ind = ["F"]
+    mis = ["G", "H", "I", "M", "P", "Q", "T", "U", "V", "W", "Y", "Z"]
+    for cc in lc.classn_r.values():
+        for v in res:
+            if cc.startswith(v):
+                arbclass[cc] = 1
+        for v in com:
+            if cc.startswith(v):
+                arbclass[cc] = 2
+        for v in mix:
+            if cc.startswith(v):
+                arbclass[cc] = 3
+        for v in ind:
+            if cc.startswith(v):
+                arbclass[cc] = 4
+        for v in mis:
+            if cc.startswith(v):
+                arbclass[cc] = 5
+
+    return arbclass
+
+
+def plot_arbclass(lc):
+    """Plot higher level building classification.
+    Args:
+        lc (obj) - LightCurve object.
+    """
+
+    # -- Define required paths.
+    bldgclass_path = os.path.join(lc.outp, "bldgclass.npy")
+    arbimg_path = os.path.join(lc.outp, "arbimg.npy")
+
+    arbclass = create_arbclass_dict(lc)
+    narbclass = {lc.classn[k]: v for k, v in arbclass.items()}
+    bldgclass = np.load(bldgclass_path)
+
+    # -- Map bldgclass to arbclass.
+    try:
+        arb_img = np.load(arbimg_path)
+    except:
+        print("arbimg.py does not exist!!")
+        arb_img = np.array([narbclass[cc] if cc in narbclass.keys() else -1
+            for cc in bldgclass.ravel()]) \
+            .reshape(bldgclass.shape[0], bldgclass.shape[1])
+
+    arb_img = arb_img.astype(float)
+    arb_img[arb_img == -1] = np.nan
+
+    # -- Plot img.
+    fig, ax = plt.subplots(figsize=(16, 8))
+    im = ax.imshow(arb_img, cmap="tab20b")
+    ax.set_title("Building Class")
+    ax.set_facecolor("w")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.grid("off")
+
+    # -- Create legend.
+    labs = ["Residential", "Commmercial", "Mixed Use", "Industrial", "Misc."]
+    values = np.unique(arb_img)
+    values = values[values > 0]
+    colors = [im.cmap(im.norm(value)) for value in values]
+    patches = [mpatches.Patch(color=colors[int(i) - 1],
+        label="{}".format(labs[int(i) - 1])) for i in values]
+    plt.legend(handles=patches, ncol=17, frameon=False)
+    plt.tight_layout()
+    plt.show()
