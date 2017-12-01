3afca6809de547ad5daaa3539a7bd753130c993e
Jordan Vani <jmv423@nyu.edu>
2017-12-01 13:54:08 -0500

    Standardized attr names. Rewrote to ID sources to ignore and calculate bigoffs without prior data.

--- cuip/variability/lightcurve.py
+++ cuip/variability/lightcurve.py
@@ -15,40 +15,44 @@ except:
 
 
 class LightCurves(object):
-    def __init__(self, lpath, vpath, rpath, spath, outp):
+    def __init__(self, lpath, vpath, rpath, spath, opath):
         """"""
 
         # -- Data paths.
-        self.lpath = lpath
-        self.vpath = vpath
-        self.rpath = rpath
-        self.spath = spath
-        self.outp = outp
+        self.path_light = lpath
+        self.path_varia = vpath
+        self.path_regis = rpath
+        self.path_suppl = spath
+        self.path_outpu = opath
 
         # -- Create metadata df.
-        self._metadata(self.rpath, os.path.join(self.outp, "null_data.pkl"))
-
-        # -- Load bigoffs if they have been saved to file.
-        bigo_path = os.path.join(self.outp, "bigoffs.pkl")
-
-        if os.path.isfile(bigo_path):
-            self._load_bigoffs(bigo_path)
-        else:
-            self._write_bigoffs(bigo_path) # Est. Time: 7 mins.lc.
-
-        # -- Load appertures.
-        self._load_wins(os.path.join(self.spath, "window_labels.out"))
-
-        # -- Find apperture centers.
-        bbl_path = os.path.join(self.spath, "12_3_14_bblgrid_clean.npy")
-        self._src_centers(bbl_path)
+        self._metadata(self.path_regis)
 
         # -- Create bbl dicts.
-        pluto_mn = os.path.join(self.spath, "pluto", "MN.csv")
+        bbl_path = os.path.join(self.path_suppl, "12_3_14_bblgrid_clean.npy")
+        pluto_mn = os.path.join(self.path_suppl, "pluto", "MN.csv")
         self._bbl_dicts(bbl_path, pluto_mn)
 
+        # -- Load srcs to be ignored.
+        null_path = os.path.join(self.path_outpu, "null_data.pkl")
+        self._ign_srcs(null_path)
 
-    def _metadata(self, rpath, null_path):
+        # -- Load appertures.
+        self._load_wins(os.path.join(self.path_suppl, "window_labels.out"))
+
+        # -- Find apperture centers.
+        self._src_centers(bbl_path)
+
+        # -- Load bigoffs if they have been saved to file.
+        path_bigoff = os.path.join(self.path_outpu, "bigoffs.pkl")
+
+        if os.path.isfile(path_bigoff):
+            self._load_bigoffs(path_bigoff)
+        else:
+            self._write_bigoffs(path_bigoff) # Est. Time: 7 mins.lc.
+
+
+    def _metadata(self, rpath):
         """Load all registration files and concatenate to pandas df. Find
         indices for every day in corresponding on/off/lightcurve files.
         Args:
@@ -58,12 +62,6 @@ class LightCurves(object):
         tstart = time.time()
         print("LIGHTCURVES: Creating metadata...                              ")
         sys.stdout.flush()
-
-        # -- Load null values data.
-        tmp = pd.DataFrame.from_dict(pd.read_pickle(null_path), orient="index")
-        tmp = tmp[tmp.null_per < 0.25]
-        # -- Extract null sources to be ignored.
-        self.ign_src = list(set(src for l in tmp.null_scr for src in l))
 
         # -- Create metadata dfs from each registration file.
         dfs = []
@@ -76,8 +74,7 @@ class LightCurves(object):
             hours = reg.timestamp.dt.hour
             reg = reg[(hours > 20) | (hours < 12)].reset_index()
             # -- Shift data so each continuours night shares a common date.
-            tdelta = datetime.timedelta(0, 6 * 3600)
-            reg.timestamp = reg.timestamp - tdelta
+            reg.timestamp = reg.timestamp - datetime.timedelta(0, 6 * 3600)
             # -- Find min and max idx for each day.
             grpby = reg.groupby(reg.timestamp.dt.date)["index"]
             min_idx = pd.DataFrame(grpby.min())
@@ -90,7 +87,7 @@ class LightCurves(object):
             dfs.append(meta)
 
         # -- Concatenate metadata dataframe, only keeping days with < 25% null.
-        self.meta = pd.concat(dfs).loc[tmp.index]
+        self.meta = pd.concat(dfs)#.loc[tmp.index]
 
         print("LIGHTCURVES: Complete ({:.2f}s)                               " \
             .format(time.time() - tstart))
@@ -132,7 +129,6 @@ class LightCurves(object):
             self.bigoffs[dd] = self._find_bigoffs()
 
         self._bigoffs_df()
-        self.bigoffs.columns = self.bigoffs.columns + 1
 
         print("LIGHTCURVES: Writing bigoffs to file...                        ")
         sys.stdout.flush()
@@ -150,9 +146,9 @@ class LightCurves(object):
         """
 
         # -- Get data for the given night.
-        dimg = self.lightc.T
+        dimg = self.src_lightc.T
         dimg[dimg == -9999.] = np.nan
-        offs = self.off.T
+        offs = self.src_offs.T
 
         # -- Loop through all sources (i.e., light curves) and find bigoff.
         bigoffs = []
@@ -186,10 +182,61 @@ class LightCurves(object):
         self.bigoffs = pd.concat(dfs)
         self.bigoffs.index = pd.to_datetime(self.bigoffs.index)
         self.bigoffs.replace(0., np.nan, inplace=True)
+        self.bigoffs.columns = self.bigoffs.columns + 1
 
         print("LIGHTCURVES: Complete ({:.2f}s)                               " \
             .format(time.time() - tstart))
         sys.stdout.flush()
+
+
+    def _ign_srcs(self, null_path):
+        """"""
+
+        cols = ["dd", "null_sources", "null_percent"]
+        # -- Load null path.
+        if os.path.isfile(null_path):
+            null_df = pd.read_pickle(null_path)
+        else:
+            tstart = time.time()
+            print("LIGHTCURVES: Finding sources that are not in all imgs...   ")
+            sys.stdout.flush()
+
+            dd_ign = []
+            for dd, row in self.meta.iterrows():
+                self.loadnight(dd)
+                dd_ign.append([dd, self.null_sources, self.null_percent])
+            null_df = pd.DataFrame(dd_ign, columns=cols).set_index("dd")
+            null_df.to_pickle(null_path)
+
+            print("LIGHTCURVES: Complete ({:.2f}s)                           " \
+                .format(time.time() - tstart))
+            sys.stdout.flush()
+
+        # -- Entirely ignore all days with more than 25% null data.
+        null_df = null_df[null_df.null_percent < 0.25]
+        # -- Extract null sources to be ignored.
+        self.src_ignore = list(set(src for l in null_df.null_sources
+            for src in l))
+
+
+    def _find_err_data(self):
+        """Find sources (indices) that are null and total null percentage for a
+        given night."""
+
+        # -- Find sources included in frame.
+        # -- Calculate the mean over all sources.
+        smean = self.src_lightc.mean(axis=0)
+        # -- Find sources that are all null (i.e., out of frame).
+        outframe = smean == -9999
+        if sum(outframe) > 0:
+            # -- Find the idx of all such sources.
+            null_src = [i for i, x in enumerate(outframe) if x == True]
+        else:
+            null_src = []
+
+        self.null_sources = null_src
+        self.null_percent = (float(sum(sum(self.src_lightc == -9999))) /
+                             self.src_lightc.size)
 
 
     def _load_wins(self, inpath):
@@ -207,20 +254,21 @@ class LightCurves(object):
         buff = 20
 
         # - Load light sources.
-        self.srcs = np.zeros((nrow, ncol), dtype=bool)
-        self.srcs[buff:-buff, buff:-buff] = np.fromfile(inpath, int) \
+        self.mat_srcs = np.zeros((nrow, ncol), dtype=bool)
+        self.mat_srcs[buff:-buff, buff:-buff] = np.fromfile(inpath, int) \
             .reshape(nrow - 2 * buff, ncol - 2 * buff) \
             .astype(bool)
 
         # -- Create label map.
-        self.labs = ndm.label(self.srcs)[0]
+        self.mat_labs = ndm.label(self.mat_srcs)[0]
         # -- Replace sources to be ignored.
-        shp1, shp2 = self.labs.shape
-        self.labs[np.in1d(self.labs, self.ign_src).reshape(shp1, shp2)] = 0
+        shp1, shp2 = self.mat_labs.shape
+        self.mat_labs[np.in1d(self.mat_labs, self.src_ignore) \
+            .reshape(shp1, shp2)] = 0
         # -- Use replaced labels to update srcs.
-        self.srcs = self.labs > 0
+        self.mat_srcs = self.mat_labs > 0
         # -- Re-label
-        self.labs = ndm.label(self.srcs)[0]
+        self.mat_labs = ndm.label(self.mat_srcs)[0]
 
         print("LIGHTCURVES: Complete ({:.2f}s)                               " \
             .format(time.time() - tstart))
@@ -238,18 +286,23 @@ class LightCurves(object):
         sys.stdout.flush()
 
         # -- List of labels to find center for.
-        labrange = range(1, self.labs.max() + 1)
+        labrange = range(1, self.mat_labs.max() + 1)
         # -- Find center coordinates.
-        xy = ndm.center_of_mass(self.srcs, self.labs, labrange)
+        xy = ndm.center_of_mass(self.mat_srcs, self.mat_labs, labrange)
         # -- Create dict of labels: (xx, yy)
-        self.coords = {self.labs[int(coord[0])][int(coord[1])]:
+        self.coords = {self.mat_labs[int(coord[0])][int(coord[1])]:
             (int(coord[1]), int(coord[0])) for coord in xy}
 
         # -- Find bbl for each source.
         bbls = np.load(bbl_path)
         xx, yy = zip(*self.coords.values())
         coord_bbls = [bbls[yy[ii] - 20][xx[ii] - 20] for ii in range(len(xx))]
-        self.coord_bbls = dict(zip(self.coords.keys(), coord_bbls))
+        self.coords_bbls = dict(zip(self.coords.keys(), coord_bbls))
+
+        # -- Find higher level bldgclass for all lightsources.
+        bbl_arb = {k: self.dd_bldg_n2[v] for k, v in self.dd_bbl_bldg.items()}
+        ccs = filter(lambda x: x[1] in bbl_arb.keys(), self.coords_bbls.items())
+        self.coords_cls = {k: bbl_arb[v] for k, v in ccs}
 
         print("LIGHTCURVES: Complete ({:.2f}s)                               " \
             .format(time.time() - tstart))
@@ -268,34 +321,47 @@ class LightCurves(object):
         df = df[[isinstance(ii, str) for ii in df.BldgClass]]
 
         # -- Create dict of BBL:BldgClass
-        self.bbl_class = {bbl: df.loc[bbl].BldgClass for bbl in
+        self.dd_bbl_bldg = {bbl: df.loc[bbl].BldgClass for bbl in
             filter(lambda x: x in df.index, np.unique(bbls))}
         # -- Create dict of BldgClass:num.
-        uniq_class = np.unique(self.bbl_class.values())
-        self.classn = dict(zip(uniq_class, range(len(uniq_class))))
-        self.classn_r = {v: k for k, v in self.classn.items()}
+        uniq_class = np.unique(self.dd_bbl_bldg.values())
+        self.dd_bldg_n1 = dict(zip(uniq_class, range(len(uniq_class))))
+        self.dd_bldg_n1_r = {v: k for k, v in self.dd_bldg_n1.items()}
         # -- Create dict of BBL:num.
-        self.bbln = dict(zip(self.bbl_class.keys(),
-            [self.classn[v] for v in self.bbl_class.values()]))
+        self.dd_bbl_n = dict(zip(self.dd_bbl_bldg.keys(),
+            [self.dd_bldg_n1[v] for v in self.dd_bbl_bldg.values()]))
+        # -- Create dict of BldgClass:
+        self._create_arbclass_dict()
 
 
-    def _find_err_data(self):
-        """Find sources (indices) that are null and total null percentage for a
-        given night."""
+    def _create_arbclass_dict(self):
+        """Categorize BldgClass numbers.
+        """
 
-        # -- Find sources included in frame.
-        # -- Calculate the mean over all sources.
-        smean = self.lightc.mean(axis=0)
-        # -- Find sources that are all null.
-        outframe = smean == -9999
-        if sum(outframe) > 0:
-            # -- Find the idx of all such sources.
-            null_src = [i for i, x in enumerate(outframe) if x == True]
-        else:
-            null_src = []
+        arbclass = {}
+        res = ["B", "C", "D", "N", "R1", "R2", "R3", "R4", "S"]
+        com = ["J", "K", "L", "O", "RA", "RB", "RC", "RI"]
+        mix = ["RM", "RR", "RX"]
+        ind = ["F"]
+        mis = ["G", "H", "I", "M", "P", "Q", "T", "U", "V", "W", "Y", "Z"]
+        for cc in self.dd_bldg_n1_r.values():
+            for v in res:
+                if cc.startswith(v):
+                    arbclass[cc] = 1
+            for v in com:
+                if cc.startswith(v):
+                    arbclass[cc] = 2
+            for v in mix:
+                if cc.startswith(v):
+                    arbclass[cc] = 3
+            for v in ind:
+                if cc.startswith(v):
+                    arbclass[cc] = 4
+            for v in mis:
+                if cc.startswith(v):
+                    arbclass[cc] = 5
 
-        self.null_src = null_src
-        self.null_per = float(sum(sum(self.lightc == -9999))) / self.lightc.size
+        self.dd_bldg_n2 = arbclass
 
 
     def _loadfiles(self, fname, start, end):
@@ -309,19 +375,23 @@ class LightCurves(object):
         """
 
         # -- Assign filepaths.
-        lc_fpath = os.path.join(self.lpath, "light_curves_{}.npy".format(fname))
-        ons_fpath  = os.path.join(self.vpath, "good_ons_{}.npy".format(fname))
-        offs_fpath = os.path.join(self.vpath, "good_offs_{}.npy".format(fname))
+        lc_fname = "light_curves_{}.npy".format(fname)
+        ons_fname = "good_ons_{}.npy".format(fname)
+        offs_fname = "good_offs_{}.npy".format(fname)
+        lc_fpath = os.path.join(self.path_light, lc_fname)
+        ons_fpath  = os.path.join(self.path_varia, ons_fname)
+        offs_fpath = os.path.join(self.path_varia, offs_fname)
 
         # -- Load .npy files and select for the correct night.
         dimg = np.load(lc_fpath).mean(-1)[start: end]
         ons = np.load(ons_fpath)[start: end]
         offs = np.load(offs_fpath)[start: end]
 
-        # -- Remove sources that are not in all images.
-        dimg = np.delete(dimg, self.ign_src, axis=1)
-        ons = np.delete(ons, self.ign_src, axis=1)
-        offs = np.delete(offs, self.ign_src, axis=1)
+        if hasattr(self, "src_ignore"):
+            # -- Remove sources that are not in all images.
+            dimg = np.delete(dimg, self.src_ignore, axis=1)
+            ons = np.delete(ons, self.src_ignore, axis=1)
+            offs = np.delete(offs, self.src_ignore, axis=1)
 
         return [dimg, ons, offs]
 
@@ -347,7 +417,8 @@ class LightCurves(object):
             fname = metadata[0]["fname"]
             start = metadata[0]["start"]
             end = metadata[0]["end"]
-            self.lightc, self.on, self.off = self._loadfiles(fname, start, end)
+            self.src_lightc, self.src_ons, self.src_offs = self._loadfiles(
+                fname, start, end)
 
         if len(metadata) == 2:
             data = []
@@ -358,9 +429,9 @@ class LightCurves(object):
                 data.append(self._loadfiles(fname, start, end))
 
             data = zip(data[0], data[1])
-            self.lightc = np.concatenate(data[0], axis=0)
-            self.on = np.concatenate(data[1], axis=0)
-            self.off = np.concatenate(data[2], axis=0)
+            self.src_lightc = np.concatenate(data[0], axis=0)
+            self.src_ons = np.concatenate(data[1], axis=0)
+            self.src_offs = np.concatenate(data[2], axis=0)
 
         self._find_err_data()
 
@@ -380,4 +451,4 @@ if __name__ == "__main__":
     lc = LightCurves(LIGH, VARI, REGI, SUPP, OUTP)
 
     # -- Load a specific night for plotting.
-    lc.loadnight(pd.datetime(2014, 7, 29).date())
+    lc.loadnight(pd.datetime(2014, 7, 13).date())
