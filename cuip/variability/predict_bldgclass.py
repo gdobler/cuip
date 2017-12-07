1457b1c11fff1fb2447030bfa13f0948062d1bb9
Jordan Vani <jmv423@nyu.edu>
2017-12-07 14:23:56 -0500

    Tried a bottom half, left/right train/test split. And predicted to the bbl.

--- cuip/variability/predict_bldgclass.py
+++ cuip/variability/predict_bldgclass.py
@@ -1,15 +1,16 @@
-from __future__ import print_function, absolute_import, division
+from __future__ import print_function
 
-import numbers
 import numpy as np
 import pandas as pd
 from fastdtw import fastdtw
 from sklearn.cluster import KMeans
-from collections import defaultdict
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import silhouette_score, confusion_matrix
 from scipy.ndimage.filters import gaussian_filter1d, gaussian_filter
+from tsfresh import extract_features
+from tsfresh.feature_extraction.settings import EfficientFCParameters
 
 
 def stack_nights():
@@ -73,8 +74,6 @@ def dtw_cluster(mean_gaus_mm, ts_mm):
 
 def use_tsfresh():
     """"""
-    from tsfresh import extract_features
-    from tsfresh.feature_extraction.settings import EfficientFCParameters
 
     param_dict = EfficientFCParameters()
     for key in ["fft_coefficient", "change_quantiles", "cwt_coefficients"]:
@@ -119,3 +118,94 @@ def use_tsfresh():
     clf.fit(xtrain[fpreds], ytrain["residential"])
     testpred = clf.predict(xtest[fpreds])
     print(confusion_matrix(ytest["residential"], testpred))
+
+
+def bottom_left_right_split(lc):
+    """Pull coords that are in the bottom half of the frame and split into
+    left and right sets.
+    Args:
+        lc (obj) - LightCurves object.
+    Returns:
+        (list) - [lclass, rclass] dictionaries of {coord: class}.
+    """
+
+    # -- Select all sources in the bottom half of the image.
+    bcoords = filter(lambda x: x[1][1] >= 1020, lc.coords.items())
+    # -- Split the bottom half into left and right coords.
+    lcoords = filter(lambda x: x[1][0] <= 2048, bcoords)
+    rcoords = filter(lambda x: x[1][0] > 2048, bcoords)
+    # -- Get keys for the left and right coords.
+    lkeys = [val[0] for val in lcoords]
+    rkeys = [val[0] for val in rcoords]
+    # -- Create dict of {key: class} for each left right split.
+    keys = lc.coords_cls.keys()
+    lclass = {key: lc.coords_cls[key] for key in lkeys if key in keys}
+    rclass = {key: lc.coords_cls[key] for key in rkeys if key in keys}
+    # -- How many Res v. Non Res in each set.
+    lres = sum(np.array(lclass.values()) == 1)
+    lnonres = sum(np.array(lclass.values()) > 1)
+    rres = sum(np.array(rclass.values()) == 1)
+    rnonres = sum(np.array(rclass.values()) > 1)
+    print("LIGHTCURVES: Left  -- Res {}, NonRes {}                           " \
+        .format(lres, lnonres))
+    print("LIGHTCURVES: Right -- Res {}, NonRes {}                           " \
+        .format(rres, rnonres))
+
+    return [lclass, rclass]
+
+
+def left_right_pred(lclass, rclass):
+    """"""
+
+    fpreds = ['val__number_peaks__n_3', 'val__ar_coefficient__k_10__coeff_2',
+        'val__energy_ratio_by_chunks__num_segments_10__segment_focus_6',
+        'val__agg_linear_trend__f_agg_"var"__chunk_len_50__attr_"stderr"']
+
+    # -- Load tsfresh data for all sources.
+    if os.path.isfile("./tsfresh_mean_gaus_mm.csv"):
+        exfeatures = pd.read_csv("./tsfresh_mean_gaus_mm.csv")
+
+    # -- Create xx and yy data for left (train) and right (test) sources.
+    lxx = exfeatures.loc[np.array(lclass.keys()) - 1].fillna(0)
+    lyy = np.array(lclass.values())
+    rxx = exfeatures.loc[np.array(rclass.keys()) - 1].fillna(0)
+    ryy = np.array(rclass.values())
+
+    # -- Predict sources as residential or non-residential.
+    clf = RandomForestClassifier(n_estimators=1000, random_state=0,
+        class_weight="balanced")
+    clf.fit(lxx[fpreds].iloc[:50, :], (lyy == 1).astype(int)[:50])
+    testpred = clf.predict(rxx[fpreds])
+    print(confusion_matrix((ryy == 1).astype(int), testpred))
+
+
+def sources_to_bbls(lc):
+    """"""
+
+    # -- Split bbls into train/test set.
+    bbls = lc.dd_bbl_srcs.keys()
+    train_bbl, test_bbl = train_test_split(bbls, test_size=0.4, random_state=0)
+
+    # -- Load tsfresh data for all sources.
+    try:
+        if os.path.isfile("./tsfresh_mean_gaus_mm.csv"):
+            exfeatures = pd.read_csv("./tsfresh_mean_gaus_mm.csv")
+    except:
+        raise("MISSING TSFRESH DATA!!")
+
+    # -- Get x-data for training and testing.
+    xtrain = [exfeatures.loc[lc.dd_bbl_srcs[bbl]].fillna(0).mean(axis=0).values
+        for bbl in train_bbl]
+    xtest = [exfeatures.loc[lc.dd_bbl_srcs[bbl]].fillna(0).mean(axis=0).values
+        for bbl in test_bbl]
+
+    # -- Get y-data for training and testing.
+    ytrain = (np.array([lc.dd_bbl_bldg2[bbl] for bbl in train_bbl]) == 1).astype(int)
+    ytest = (np.array([lc.dd_bbl_bldg2[bbl] for bbl in test_bbl]) == 1).astype(int)
+
+    # -- Predict buildings as residential or non-residential.
+    clf = RandomForestClassifier(n_estimators=1000, random_state=0,
+        class_weight="balanced")
+    clf.fit(xtrain, ytrain)
+    testpred = clf.predict(xtest)
+    print(confusion_matrix(ytest, testpred))
