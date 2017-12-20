320b9c22de6fa5797824d4203d3046c38bd1b38f
Jordan Vani <jmv423@nyu.edu>
2017-12-20 10:02:58 -0700

    Revise histogram matching.

--- cuip/variability/histogram_matching.py
+++ cuip/variability/histogram_matching.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage.data import chelsea, coffee
@@ -7,77 +8,77 @@ from skimage.data import chelsea, coffee
 plt.style.use("ggplot")
 
 
-def single_channel_histogram(channel, bins=256):
-    """Calculate channels hist params.
+def single_channel_cdf(channel):
+    """Calculate channel's cdf.
     Args:
         Channel (array) - 2D image channel.
     Returns:
-        params (list)
+        cdf (array) - Normalized cdf (256 bins).
     """
-    shape = channel.shape
-    # -- Summarize channel.
-    chan_vals, chan_bins, chan_cnts = np.unique(channel.ravel(),
-        return_inverse=True, return_counts=True)
-    # -- Find quantiles.
-    chan_quantiles = np.cumsum(chan_cnts).astype(float)
-    chan_quantiles /= chan_quantiles[-1]
+    # -- Calculate cdf.
+    cdf = np.cumsum(np.histogram(channel.flatten(), 256, [0, 255])[0])
+    # -- Normalize.
+    cdf = cdf.astype(float) / cdf.max()
 
-    return [chan_vals, chan_bins, chan_cnts, chan_quantiles, shape]
+    return cdf
 
 
-def single_channel_match(img_params, ref_params):
-    """Match channels from image to reference.
+def single_channel_vals(img_cdf, ref_cdf):
+    """Map values from img_cdf to ref_cdf.
     Args:
-        img_params (list) - image params.
-        ref_params (list) - reference params.
+        ref_cdf (array) - reference channel cdf.
+        img_cdf (array) - target channel cdf.
     Returns:
-        (array) - 2D array of of matched channel.
+        vals (array) - mapped values for 256 values.
     """
-    # -- Unpack params.
-    img_vals, img_bins, img_cnts, img_quantiles, img_shape = img_params
-    ref_vals, ref_bins, ref_cnts, ref_quantiles, ref_shape = ref_params
-    # -- Interpolate images quantiles to match reference.
-    interp_vals = np.interp(img_quantiles, ref_quantiles, ref_vals)
-    # -- Set interpolated values.
-    interp_channel = interp_vals[img_bins].reshape(img_shape).astype(int)
+    # -- Map img values to ref.
+    vals = [list(ref_cdf >= img_cdf[ii]).index(True) for ii in range(len(img_cdf))]
 
-    return interp_channel.astype(float)
+    return np.array(vals).astype(float)
 
 
-def rgb_params(im):
-    """Calculate RGB histograms.
+def single_channel_match(channel, vals):
+    """Replace values in channel with mapped values.
     Args:
-        im (array) - RGB image.
+        channel (array) - image channel.
+        vals (array) - mapped values for 256 values.
     Returns:
-        (array) - RGB params.
+        match (array) - image with remapped values.
     """
-    # -- Calculate RGB params.
-    return [single_channel_histogram(im[:, :, ii]) for ii in range(3)]
+    # -- Empty array to fill with mapped values.
+    match = np.zeros(channel.shape, dtype=np.uint8)
+    # -- Map all values in channel to matched values.
+    for ii in range(256):
+        match[channel == ii] = vals[ii]
+
+    return match
 
 
-def rgb_match(img_params, ref_params):
-    """Perform histogram matching for a 3 channel RGB image.
+def rgb_match(img, ref):
+    """Histogram matching for 3 color images.
     Args:
-        img (array) - RGB image.
-        ref (array) - RGB reference image.
+        img (array) - target image.
+        ref (array) - reference image.
     Returns:
-        new_channels (array) - RGB histogram matched channels.
+        match (array) - histogram matched target image.
     """
-    # -- Match each channel.
-    params = zip(img_params, ref_params)
-    new_channels = np.array([single_channel_match(im, rr) for im, rr in params])
-    # -- Inverse colors.
-    new_channels = np.array([256, 256, 256]) - np.moveaxis(new_channels, 0, -1)
+    # -- Find cdfs.
+    img_cdfs = [single_channel_cdf(img[:, :, ii]) for ii in range(3)]
+    ref_cdfs = [single_channel_cdf(ref[:, :, ii]) for ii in range(3)]
+    # -- Find value map.
+    vals = [single_channel_vals(ii, rr) for ii, rr in zip(img_cdfs, ref_cdfs)]
+    # -- Map image values.
+    match = [single_channel_match(img[:, :, ii], vals[ii]) for ii in range(3)]
 
-    return new_channels
+    return np.moveaxis(match, 0, -1)
 
 
-def plot_match(img, ref, new_channels, figsize=(6, 8)):
+def plot_match(img, ref, match, figsize=(6, 8)):
     """Plot image, reference, and resulting matches.
     Args:
         img (array) - RGB image.
         ref (array) - RGB reference image.
-        new_channels (array) - RGB histogram matched channels.
+        match (array) - RGB histogram matched image
     """
     # -- Create figure.
     fig, [r1, r2, r3, r4] = plt.subplots(nrows=4, ncols=3, figsize=figsize)
@@ -86,11 +87,11 @@ def plot_match(img, ref, new_channels, figsize=(6, 8)):
         if ii < 3:
             ref_ax.imshow(ref[:, :, ii], cmap="gray")
             img_ax.imshow(img[:, :, ii], cmap="gray")
-            new_ax.imshow(256 - new_channels[:, :, ii], cmap="gray")
+            new_ax.imshow(match[:, :, ii], cmap="gray")
         else:
             ref_ax.imshow(ref)
             img_ax.imshow(img)
-            new_ax.imshow(new_channels)
+            new_ax.imshow(match)
     # -- Axes labels.
     for ax, label in zip([r1[0], r2[0], r3[0], r4[0]], ["R", "G", "B", "Color"]):
         ax.set_ylabel(label)
@@ -111,11 +112,42 @@ def demo(img=coffee(), ref=chelsea(), figsize=(6, 6)):
         ref (array) - RGB image to use as reference.
         figsize (tup) - plotting size.
     """
-    img_params = rgb_params(img)
+    match = rgb_match(img, ref)
+    plot_match(img, ref, match, figsize=figsize)
+
+
+def match_lightcurves(lc):
+    """Perform histogram matching for all lightcurve files.
+    Args:
+        lc (obj) - LightCurve object.
+    """
+    # -- Load registration files to df.
+    fnames = sorted(os.listdir(lc.path_regis))
+    fpaths = [os.path.join(lc.path_regis, fname) for fname in fnames]
+    reg = pd.concat([pd.read_csv(path) for path in fpaths])
+    # -- Get lightcurve paths.
+    fnames = sorted(os.listdir(lc.path_light))
+    fpaths = [os.path.join(lc.path_light, fname) for fname in fnames]
+    # -- Load reference "image" and set reference params.
+    tmp = np.load(fpaths[-1])
+    ref = np.ma.array(tmp[-5, :, :], mask=tmp[-5, :, :] == -9999)
     ref_params = rgb_params(ref)
-    new_channels = rgb_match(img_params, ref_params)
-    plot_match(img, ref, new_channels, figsize=figsize)
+    # -- For all lightcurves, histogram match each night to a reference time.
+    for ii, fpath in enumerate(fpaths):
+        bname = os.path.basename(fpath)
+        print("LIGHTCURVES: Histogram matching {} ({}/{})                    " \
+            .format(bname, ii + 1, len(fpaths)), end="\r")
+        sys.stdout.flush()
+        # -- Load lightcurves.
+        lightc = np.load(fpath)
+        malightc = np.ma.array(lightc, mask=lightc == -9999)
+        # -- Histogram matching.
+        mlight = np.array([rgb_match(rgb_params(src), ref_params) for src in malightc])
+        # -- Save new lightcurve array.
+        hpath = os.path.join(lc.path_outpu, "histogram_matching", bname)
+        np.save(hpath, mlight)
 
 
 if __name__ == "__main__":
     demo()
+    # match_lightcurves(lc)
